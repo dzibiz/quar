12279f375e1d4f6bc6ac258eee02ad9251317b95
dzibiz <48919944+dzibiz@users.noreply.github.com>
2021-01-19 13:41:21 +0500

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60069ae3e84d4f001e3930c2",
+				"60069ae3e84d4f001e3930c4"
+			]
+		},
+		"60069ae3e84d4f001e3930c2": {
+			"id": "60069ae3e84d4f001e3930c2",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60069ae3e84d4f001e3930c4": {
+			"id": "60069ae3e84d4f001e3930c4",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
